e1a346eb5d61446bb489686a56fc4d385b413928
Drew Minns <drew@reallygood.work>
2024-05-28 12:13:53 -0400

    fixes deploy

--- apps/site/sanity.config.ts
+++ apps/site/sanity.config.ts
@@ -1,15 +1,27 @@
-import { defineConfig } from "sanity";
-import { structureTool } from "sanity/structure";
 import { visionTool } from '@sanity/vision'
-import {sanityComponents} from './src/sanity';
+import { defineConfig } from 'sanity'
+import { vercelDeployTool } from 'sanity-plugin-vercel-deploy'
+import { presentationTool } from 'sanity/presentation'
+import { structureTool } from 'sanity/structure'
+
+import { sanityComponents } from './src/sanity'
+
+const SANITY_STUDIO_PREVIEW_URL = import.meta.env.SANITY_STUDIO_PREVIEW_URL || 'http://localhost:1234'
 
 export default defineConfig({
-  name: "default",
-  title: "Really Good Work Inc.",
+  name: 'default',
+  title: 'Really Good Work Inc.',
   projectId: 'e9frm8yx',
   dataset: 'production',
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    structureTool(),
+    visionTool(),
+    presentationTool({
+      previewUrl: SANITY_STUDIO_PREVIEW_URL,
+    }),
+    vercelDeployTool(),
+  ],
   schema: {
     types: sanityComponents,
   },
-});
+})
